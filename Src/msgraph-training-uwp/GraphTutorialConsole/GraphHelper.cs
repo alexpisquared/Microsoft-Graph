2fac3242f6ffbe0885ec26c254424c19295d57f9
alexpisqueared <alex.pigida@outlook.com>
2023-05-13 21:33:11 -0400

    POC of photo access ... to elaborate, needs more wits/time.

--- Src/msgraph-training-uwp/GraphTutorialConsole/GraphHelper.cs
+++ Src/msgraph-training-uwp/GraphTutorialConsole/GraphHelper.cs
@@ -1,172 +1,198 @@
-﻿using Microsoft.Graph;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Graph;
 using TimeZoneConverter;
 
-namespace GraphTutorial
+namespace GraphTutorialConsole;
+
+public class GraphHelper
 {
-  public class GraphHelper
+  static GraphServiceClient graphServiceClient;
+  public static void Initialize(IAuthenticationProvider authProvider) => graphServiceClient = new GraphServiceClient(authProvider);
+
+  public static async Task<User> GetMeAsync()
   {
-    static GraphServiceClient graphClient;
-    public static void Initialize(IAuthenticationProvider authProvider) => graphClient = new GraphServiceClient(authProvider);
-
-    public static async Task<User> GetMeAsync()
+    try
     {
-      try
-      {
-        // GET /me
-        return await graphClient.Me
-            .Request()
-            .Select(u => new
-            {
-              u.DisplayName,
-              u.MailboxSettings
-            })
-            .GetAsync();
-      }
-      catch (ServiceException ex)
-      {
-        Console.WriteLine($"Error getting signed-in user: {ex.Message}");
-        return null;
-      }
+      // GET /me
+      return await graphServiceClient.Me
+          .Request()
+          .Select(u => new
+          {
+            u.DisplayName,
+            u.MailboxSettings
+          })
+          .GetAsync();
     }
-
-    public static async Task<IEnumerable<Event>> GetCurrentWeekCalendarViewAsync(
-      DateTime today,
-      string timeZone)
+    catch (ServiceException ex)
     {
-      // Configure a calendar view for the current week
-      var startOfWeek = GetUtcStartOfWeekInTimeZone(today, timeZone);
-      var endOfWeek = startOfWeek.AddDays(7);
+      Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+      return null;
+    }
+  }
 
-      var viewOptions = new List<QueryOption>
+  public static async Task<IEnumerable<Event>> GetCurrentWeekCalendarViewAsync(DateTime today, string timeZone)
+  {
+    // Configure a calendar view for the current week
+    var startOfWeek = GetUtcStartOfWeekInTimeZone(today, timeZone);
+    var endOfWeek = startOfWeek.AddDays(7);
+
+    var viewOptions = new List<QueryOption>
     {
-        new QueryOption("startDateTime", startOfWeek.ToString("o")),
-        new QueryOption("endDateTime", endOfWeek.ToString("o"))
+      new QueryOption("startDateTime", startOfWeek.ToString("o")),
+      new QueryOption("endDateTime", endOfWeek.ToString("o"))
     };
 
-      try
-      {
-        var events = await graphClient.Me
-            .CalendarView
-            .Request(viewOptions)
-            // Send user time zone in request so date/time in
-            // response will be in preferred time zone
-            .Header("Prefer", $"outlook.timezone=\"{timeZone}\"")
-            // Get max 50 per request
-            .Top(50)
-            // Only return fields app will use
-            .Select(e => new
-            {
-              e.Subject,
-              e.Organizer,
-              e.Start,
-              e.End
-            })
-            // Order results chronologically
-            .OrderBy("start/dateTime")
-            .GetAsync();
-
-        return events.CurrentPage;
-      }
-      catch (ServiceException ex)
-      {
-        Console.WriteLine($"Error getting events: {ex.Message}");
-        return null;
-      }
-    }
-
-    static DateTime GetUtcStartOfWeekInTimeZone(DateTime today, string timeZoneId)
+    try
     {
-      // Time zone returned by Graph could be Windows or IANA style
-      // .NET Core's FindSystemTimeZoneById needs IANA on Linux/MacOS,
-      // and needs Windows style on Windows.
-      // TimeZoneConverter can handle this for us
-      var userTimeZone = TZConvert.GetTimeZoneInfo(timeZoneId);
+      var events = await graphServiceClient.Me
+          .CalendarView
+          .Request(viewOptions)
+          // Send user time zone in request so date/time in
+          // response will be in preferred time zone
+          .Header("Prefer", $"outlook.timezone=\"{timeZone}\"")
+          // Get max 50 per request
+          .Top(50)
+          // Only return fields app will use
+          .Select(e => new
+          {
+            e.Subject,
+            e.Organizer,
+            e.Start,
+            e.End
+          })
+          // Order results chronologically
+          .OrderBy("start/dateTime")
+          .GetAsync();
 
-      // Assumes Sunday as first day of week
-      var diff = System.DayOfWeek.Sunday - today.DayOfWeek;
-
-      // create date as unspecified kind
-      var unspecifiedStart = DateTime.SpecifyKind(today.AddDays(diff), DateTimeKind.Unspecified);
-
-      // convert to UTC
-      return TimeZoneInfo.ConvertTimeToUtc(unspecifiedStart, userTimeZone);
+      return events.CurrentPage;
     }
+    catch (ServiceException ex)
+    {
+      Console.WriteLine($"Error getting events: {ex.Message}");
+      return null;
+    }
+  }
+  public static async Task<IEnumerable<Event>> GetPhotoAsync(DateTime today, string timeZone)
+  {
+    try
+    {
+      var items = await graphServiceClient.Me.Drive.Root.Children.Request().GetAsync();
 
-    public static async Task CreateEvent(
+      dynamic iems = await graphServiceClient.Me.Drive.Root.Children.Request().GetAsync();
+      var pics = iems.Items[12].Name;
+
+      var profilePhoto = await graphServiceClient.Me.Photo.Content.Request().GetAsync();
+      if (profilePhoto != null)
+      {
+        var ms = new MemoryStream();
+        profilePhoto.CopyTo(ms);
+        var buffer = ms.ToArray();
+        var result = Convert.ToBase64String(buffer);
+        var imgDataURL = string.Format("data:image/png;base64, {0}", result);
+        //ViewBag.ImageData = imgDataURL;
+      }
+      else
+      {
+        //ViewBag.ImageData = "";
+      }
+
+      return null;
+    }
+    catch (ServiceException ex)
+    {
+      Console.WriteLine($"Error getting events: {ex.Message}");
+      return null;
+    }
+  }
+
+  static DateTime GetUtcStartOfWeekInTimeZone(DateTime today, string timeZoneId)
+  {
+    // Time zone returned by Graph could be Windows or IANA style
+    // .NET Core's FindSystemTimeZoneById needs IANA on Linux/MacOS,
+    // and needs Windows style on Windows.
+    // TimeZoneConverter can handle this for us
+    var userTimeZone = TZConvert.GetTimeZoneInfo(timeZoneId);
+
+    // Assumes Sunday as first day of week
+    var diff = System.DayOfWeek.Sunday - today.DayOfWeek;
+
+    // create date as unspecified kind
+    var unspecifiedStart = DateTime.SpecifyKind(today.AddDays(diff), DateTimeKind.Unspecified);
+
+    // convert to UTC
+    return TimeZoneInfo.ConvertTimeToUtc(unspecifiedStart, userTimeZone);
+  }
+
+  public static async Task CreateEvent(
     string timeZone,
     string subject,
     DateTime start,
     DateTime end,
     List<string> attendees,
     string body = null)
+  {
+    // Create a new Event object with required
+    // values
+    var newEvent = new Event
     {
-      // Create a new Event object with required
-      // values
-      var newEvent = new Event
+      Subject = subject,
+      Start = new DateTimeTimeZone
       {
-        Subject = subject,
-        Start = new DateTimeTimeZone
+        DateTime = start.ToString("o"),
+        // Set to the user's time zone
+        TimeZone = timeZone
+      },
+      End = new DateTimeTimeZone
+      {
+        DateTime = end.ToString("o"),
+        // Set to the user's time zone
+        TimeZone = timeZone
+      }
+    };
+
+    // Only add attendees if there are actual
+    // values
+    if (attendees.Count > 0)
+    {
+      var requiredAttendees = new List<Attendee>();
+
+      foreach (var email in attendees)
+        requiredAttendees.Add(new Attendee
         {
-          DateTime = start.ToString("o"),
-          // Set to the user's time zone
-          TimeZone = timeZone
-        },
-        End = new DateTimeTimeZone
-        {
-          DateTime = end.ToString("o"),
-          // Set to the user's time zone
-          TimeZone = timeZone
-        }
+          Type = AttendeeType.Required,
+          EmailAddress = new EmailAddress
+          {
+            Address = email
+          }
+        });
+
+      newEvent.Attendees = requiredAttendees;
+    }
+
+    // Only add a body if a body was supplied
+    if (!string.IsNullOrEmpty(body))
+      newEvent.Body = new ItemBody
+      {
+        Content = body,
+        ContentType = BodyType.Text
       };
 
-      // Only add attendees if there are actual
-      // values
-      if (attendees.Count > 0)
-      {
-        var requiredAttendees = new List<Attendee>();
+    try
+    {
+      // POST /me/events
+      _ = await graphServiceClient.Me
+          .Events
+          .Request()
+          .AddAsync(newEvent);
 
-        foreach (var email in attendees)
-        {
-          requiredAttendees.Add(new Attendee
-          {
-            Type = AttendeeType.Required,
-            EmailAddress = new EmailAddress
-            {
-              Address = email
-            }
-          });
-        }
-
-        newEvent.Attendees = requiredAttendees;
-      }
-
-      // Only add a body if a body was supplied
-      if (!string.IsNullOrEmpty(body))
-      {
-        newEvent.Body = new ItemBody
-        {
-          Content = body,
-          ContentType = BodyType.Text
-        };
-      }
-
-      try
-      {
-        // POST /me/events
-        await graphClient.Me
-            .Events
-            .Request()
-            .AddAsync(newEvent);
-
-        Console.WriteLine("Event added to calendar.");
-      }
-      catch (ServiceException ex)
-      {
-        Console.WriteLine($"Error creating event: {ex.Message}");
-      }
+      Console.WriteLine("Event added to calendar.");
+    }
+    catch (ServiceException ex)
+    {
+      Console.WriteLine($"Error creating event: {ex.Message}");
     }
   }
 }
--- Src/msgraph-training-uwp/GraphTutorialConsole/Program.cs
+++ Src/msgraph-training-uwp/GraphTutorialConsole/Program.cs
@@ -1,246 +1,213 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using GraphTutorial;
+using Microsoft.Extensions.Configuration;
 
-namespace GraphTutorial
+namespace GraphTutorialConsole;
+
+class Program
 {
-  class Program
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
+    Console.WriteLine(".NET Core Graph Tutorial\n");
+
+    var appConfig = LoadAppSettings();
+    if (appConfig == null)
     {
-      Console.WriteLine(".NET Core Graph Tutorial\n");
-
-      var appConfig = LoadAppSettings();
-      if (appConfig == null)
-      {
-        Console.WriteLine("Missing or invalid appsettings.json...exiting");
-        return;
-      }
-
-      var appId = appConfig["appId"];
-      var scopes = appConfig["scopes"].Split(';');
-
-      var authProvider = new DeviceCodeAuthProvider(appId, scopes); // Initialize the auth provider with values from appsettings.json
-      var accessToken0 = appConfig["AccessToken"];
-      var accessToken = accessToken0 ?? authProvider.GetAccessToken().Result;       // Request a token to sign in the user
-
-      // Initialize Graph client
-      GraphHelper.Initialize(authProvider);
-
-      // Get signed in user
-      var user = GraphHelper.GetMeAsync().Result;
-      Console.WriteLine($"Welcome {user.DisplayName}!\n");
-
-
-      int choice = -1;
-
-      while (choice != 0)
-      {
-        Console.WriteLine("Please choose one of the following options:");
-        Console.WriteLine("0. Exit");
-        Console.WriteLine("1. Display access token");
-        Console.WriteLine("2. View this week's calendar");
-        Console.WriteLine("3. Add an event");
-
-        try
-        {
-          choice = int.Parse(Console.ReadLine());
-        }
-        catch (System.FormatException)
-        {
-          // Set to invalid value
-          choice = -1;
-        }
-
-        switch (choice)
-        {
-          case 0:
-            // Exit the program
-            Console.WriteLine("Goodbye...");
-            break;
-          case 1:
-            // Display access token
-            Console.WriteLine($"Access token: {accessToken}\n");
-            break;
-          case 2:
-            // List the calendar
-            ListCalendarEvents(user.MailboxSettings.TimeZone, $"{user.MailboxSettings.DateFormat} {user.MailboxSettings.TimeFormat}");
-            break;
-          case 3:
-            // Create a new event
-            CreateEvent(user.MailboxSettings.TimeZone);
-            break;
-          default:
-            Console.WriteLine("Invalid choice! Please try again.");
-            break;
-        }
-      }
+      Console.WriteLine("Missing or invalid appsettings.json...exiting");
+      return;
     }
 
-    static IConfigurationRoot LoadAppSettings()
-    {
-      var appConfig = new ConfigurationBuilder()
-          .AddUserSecrets<Program>()
-          .Build();
+    var appId = appConfig["appId"];
+    var scopes = appConfig["scopes"].Split(';');
 
-      // Check for required settings
-      if (string.IsNullOrEmpty(appConfig["appId"]) ||
-          string.IsNullOrEmpty(appConfig["scopes"]))
+    var authProvider = new DeviceCodeAuthProvider(appId, scopes); // Initialize the auth provider with values from appsettings.json
+    var accessToken0 = appConfig["AccessToken"];
+    var accessToken = accessToken0 ?? authProvider.GetAccessToken().Result;       // Request a token to sign in the user
+
+    // Initialize Graph client
+    GraphHelper.Initialize(authProvider);
+
+    // Get signed in user
+    var user = GraphHelper.GetMeAsync().Result;
+    Console.WriteLine($"Welcome {user?.DisplayName ?? "NUL"}!\n");
+
+    var choice = -1;
+
+    while (choice != 0)
+    {
+      Console.WriteLine("Please choose one of the following options:");
+      Console.WriteLine("0. Exit");
+      Console.WriteLine("1. Display access token");
+      Console.WriteLine("2. View this week's calendar");
+      Console.WriteLine("3. Add an event");
+      Console.WriteLine("4. List PHOTO!!!!!!!!!!!!!");
+
+      try
       {
-        return null;
+        choice = int.Parse(Console.ReadLine());
+      }
+      catch (FormatException)
+      {
+        // Set to invalid value
+        choice = -1;
       }
 
-      return appConfig;
-    }
-
-    static string FormatDateTimeTimeZone(Microsoft.Graph.DateTimeTimeZone value, string dateTimeFormat)
-    {
-      // Parse the date/time string from Graph into a DateTime
-      var dateTime = DateTime.Parse(value.DateTime);
-
-      return dateTime.ToString(dateTimeFormat);
-    }
-
-    static void ListCalendarEvents(string userTimeZone, string dateTimeFormat)
-    {
-      var events = GraphHelper
-          .GetCurrentWeekCalendarViewAsync(DateTime.Today, userTimeZone)
-          .Result;
-
-      Console.WriteLine("Events:");
-
-      foreach (var calendarEvent in events)
+      switch (choice)
       {
-        Console.WriteLine($"Subject: {calendarEvent.Subject}");
-        Console.WriteLine($"  Organizer: {calendarEvent.Organizer.EmailAddress.Name}");
-        Console.WriteLine($"  Start: {FormatDateTimeTimeZone(calendarEvent.Start, dateTimeFormat)}");
-        Console.WriteLine($"  End: {FormatDateTimeTimeZone(calendarEvent.End, dateTimeFormat)}");
-      }
-    }
-
-    static bool GetUserYesNo(string prompt)
-    {
-      Console.Write($"{prompt} (y/n)");
-      ConsoleKeyInfo confirm;
-      do
-      {
-        confirm = Console.ReadKey(true);
-      }
-      while (confirm.Key != ConsoleKey.Y && confirm.Key != ConsoleKey.N);
-
-      Console.WriteLine();
-      return (confirm.Key == ConsoleKey.Y);
-    }
-
-    static string GetUserInput(string fieldName, bool isRequired, Func<string, bool> validate)
-    {
-      string returnValue = null;
-      do
-      {
-        Console.Write($"Enter a {fieldName}: ");
-        if (!isRequired)
-        {
-          Console.Write("(ENTER to skip) ");
-        }
-        var input = Console.ReadLine();
-
-        if (!string.IsNullOrEmpty(input))
-        {
-          if (validate.Invoke(input))
-          {
-            returnValue = input;
-          }
-        }
-      }
-      while (string.IsNullOrEmpty(returnValue) && isRequired);
-
-      return returnValue;
-    }
-
-    static void CreateEvent(string userTimeZone)
-    {
-      // Prompt user for info
-
-      // Require a subject
-      var subject = GetUserInput("subject", true,
-          (input) =>
-          {
-            return GetUserYesNo($"Subject: {input} - is that right?");
-          });
-
-      // Attendees are optional
-      var attendeeList = new List<string>();
-      if (GetUserYesNo("Do you want to invite attendees?"))
-      {
-        string attendee = null;
-
-        do
-        {
-          attendee = GetUserInput("attendee", false,
-              (input) =>
-              {
-                return GetUserYesNo($"{input} - add attendee?");
-              });
-
-          if (!string.IsNullOrEmpty(attendee))
-          {
-            attendeeList.Add(attendee);
-          }
-        }
-        while (!string.IsNullOrEmpty(attendee));
-      }
-
-      var startString = GetUserInput("event start", true,
-          (input) =>
-          {
-            // Validate that input is a date
-            return (DateTime.TryParse(input, out var result));
-          });
-
-      var start = DateTime.Parse(startString);
-
-      var endString = GetUserInput("event end", true,
-          (input) =>
-          {
-            // Validate that input is a date
-            // and is later than start
-            return (DateTime.TryParse(input, out var result) &&
-                result.CompareTo(start) > 0);
-          });
-
-      var end = DateTime.Parse(endString);
-
-      var body = GetUserInput("body", false,
-          (input => { return true; }));
-
-      Console.WriteLine($"Subject: {subject}");
-      Console.WriteLine($"Attendees: {string.Join(";", attendeeList)}");
-      Console.WriteLine($"Start: {start.ToString()}");
-      Console.WriteLine($"End: {end.ToString()}");
-      Console.WriteLine($"Body: {body}");
-      if (GetUserYesNo("Create event?"))
-      {
-        GraphHelper.CreateEvent(
-            userTimeZone,
-            subject,
-            start,
-            end,
-            attendeeList,
-            body).Wait();
-      }
-      else
-      {
-        Console.WriteLine("Canceled.");
+        case 0: Console.WriteLine("Goodbye..."); break;
+        case 1: Console.WriteLine($"Access token: {accessToken}\n"); break;
+        case 2: ListCalendarEvents(user.MailboxSettings.TimeZone, $"{user.MailboxSettings.DateFormat} {user.MailboxSettings.TimeFormat}"); break;
+        case 4: ListPhotos(user.MailboxSettings.TimeZone, $"{user.MailboxSettings.DateFormat} {user.MailboxSettings.TimeFormat}"); break;
+        case 3: CreateEvent(user.MailboxSettings.TimeZone); break;
+        default: Console.WriteLine("Invalid choice! Please try again."); break;
       }
     }
   }
+
+  static IConfigurationRoot LoadAppSettings()
+  {
+    var appConfig = new ConfigurationBuilder().AddUserSecrets<Program>().Build(); //tu: secrets by the book.
+
+    return string.IsNullOrEmpty(appConfig["appId"]) || string.IsNullOrEmpty(appConfig["scopes"]) ? null : appConfig;
+  }
+
+  static string FormatDateTimeTimeZone(Microsoft.Graph.DateTimeTimeZone value, string dateTimeFormat)
+  {
+    // Parse the date/time string from Graph into a DateTime
+    var dateTime = DateTime.Parse(value.DateTime);
+
+    return dateTime.ToString(dateTimeFormat);
+  }
+
+  static void ListCalendarEvents(string userTimeZone, string dateTimeFormat)
+  {
+    var events = GraphHelper.GetCurrentWeekCalendarViewAsync(DateTime.Today, userTimeZone).Result;
+
+    Console.WriteLine("Events:");
+
+    foreach (var calendarEvent in events)
+    {
+      Console.WriteLine($"Subject: {calendarEvent.Subject}");
+      Console.WriteLine($"  Organizer: {calendarEvent.Organizer.EmailAddress.Name}");
+      Console.WriteLine($"  Start: {FormatDateTimeTimeZone(calendarEvent.Start, dateTimeFormat)}");
+      Console.WriteLine($"  End: {FormatDateTimeTimeZone(calendarEvent.End, dateTimeFormat)}");
+    }
+  }
+  static void ListPhotos(string userTimeZone, string dateTimeFormat)
+  {
+    var events = GraphHelper.GetPhotoAsync(DateTime.Today, userTimeZone).Result;
+
+    Console.WriteLine("Events:");
+
+    foreach (var calendarEvent in events)
+    {
+      Console.WriteLine($"Subject: {calendarEvent.Subject}");
+      Console.WriteLine($"  Organizer: {calendarEvent.Organizer.EmailAddress.Name}");
+      Console.WriteLine($"  Start: {FormatDateTimeTimeZone(calendarEvent.Start, dateTimeFormat)}");
+      Console.WriteLine($"  End: {FormatDateTimeTimeZone(calendarEvent.End, dateTimeFormat)}");
+    }
+  }
+
+  static bool GetUserYesNo(string prompt)
+  {
+    Console.Write($"{prompt} (y/n)");
+    ConsoleKeyInfo confirm;
+    do
+      confirm = Console.ReadKey(true);
+    while (confirm.Key is not ConsoleKey.Y and not ConsoleKey.N);
+
+    Console.WriteLine();
+    return confirm.Key == ConsoleKey.Y;
+  }
+
+  static string GetUserInput(string fieldName, bool isRequired, Func<string, bool> validate)
+  {
+    string returnValue = null;
+    do
+    {
+      Console.Write($"Enter a {fieldName}: ");
+      if (!isRequired)
+        Console.Write("(ENTER to skip) ");
+      var input = Console.ReadLine();
+
+      if (!string.IsNullOrEmpty(input))
+        if (validate.Invoke(input))
+          returnValue = input;
+    }
+    while (string.IsNullOrEmpty(returnValue) && isRequired);
+
+    return returnValue;
+  }
+
+  static void CreateEvent(string userTimeZone)
+  {
+    // Prompt user for info
+
+    // Require a subject
+    var subject = GetUserInput("subject", true,
+        (input) =>
+        {
+          return GetUserYesNo($"Subject: {input} - is that right?");
+        });
+
+    // Attendees are optional
+    var attendeeList = new List<string>();
+    if (GetUserYesNo("Do you want to invite attendees?"))
+    {
+      string attendee = null;
+
+      do
+      {
+        attendee = GetUserInput("attendee", false,
+            (input) =>
+            {
+              return GetUserYesNo($"{input} - add attendee?");
+            });
+
+        if (!string.IsNullOrEmpty(attendee))
+          attendeeList.Add(attendee);
+      }
+      while (!string.IsNullOrEmpty(attendee));
+    }
+
+    var startString = GetUserInput("event start", true,
+        (input) =>
+        {
+          // Validate that input is a date
+          return DateTime.TryParse(input, out var result);
+        });
+
+    var start = DateTime.Parse(startString);
+
+    var endString = GetUserInput("event end", true,
+        (input) =>
+        {
+          // Validate that input is a date
+          // and is later than start
+          return DateTime.TryParse(input, out var result) &&
+              result.CompareTo(start) > 0;
+        });
+
+    var end = DateTime.Parse(endString);
+
+    var body = GetUserInput("body", false,
+        input => { return true; });
+
+    Console.WriteLine($"Subject: {subject}");
+    Console.WriteLine($"Attendees: {string.Join(";", attendeeList)}");
+    Console.WriteLine($"Start: {start}");
+    Console.WriteLine($"End: {end}");
+    Console.WriteLine($"Body: {body}");
+    if (GetUserYesNo("Create event?"))
+      GraphHelper.CreateEvent(
+          userTimeZone,
+          subject,
+          start,
+          end,
+          attendeeList,
+          body).Wait();
+    else
+      Console.WriteLine("Canceled.");
+  }
 }
-
-/*
-  ReadMe.md
-  .NET Core Graph Tutorial  from  https://docs.microsoft.com/en-us/graph/tutorials/dotnet-core
-  For the original see C:\gh\s\msgraph-training-dotnet-core\demo\GraphTutorial 
-
-  
-  OAuth2: https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-device-code
-*/
