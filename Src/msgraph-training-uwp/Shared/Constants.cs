1b37b00d4aaa9e739f6a3d1c43c925a29313e206
alexpisqueared <alex.pigida@outlook.com>
2023-05-16 20:55:55 -0400

    TokenCaching !!!!!!!!!!!!!!!!

---
+++ Src/msgraph-training-uwp/Shared/Constants.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if ADAL
+namespace Microsoft.Identity.Client.Extensions.Adal
+#elif MSAL
+namespace Microsoft.Identity.Client.Extensions.Msal
+#else // WEB
+namespace Microsoft.Identity.Client.Extensions.Web
+#endif
+{
+    internal class Constants
+    {
+        public const string MacKeyChainDeleteFailed = "SecKeychainItemDelete failed with error code: {0}";
+        public const string MacKeyChainFindFailed = "SecKeychainFindGenericPassword failed with error code: {0}";
+        public const string MacKeyChainInsertFailed = "SecKeychainAddGenericPassword failed with error code: {0}";
+        public const string MacKeyChainUpdateFailed = "SecKeychainItemModifyAttributesAndData failed with error code: {0}";
+    }
+}
---
+++ Src/msgraph-training-uwp/Shared/Linux/LinuxNativeMethods.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+#if ADAL
+namespace Microsoft.Identity.Client.Extensions.Adal
+#elif MSAL
+namespace Microsoft.Identity.Client.Extensions.Msal
+#else // WEB
+namespace Microsoft.Identity.Client.Extensions.Web
+#endif
+{
+internal static class LinuxNativeMethods
+    {
+        public const int RootUserId = 0;
+
+        /// <summary>
+        /// Get the real user ID of the calling process.
+        /// </summary>
+        /// <returns>the real user ID of the calling process</returns>
+        [DllImport("libc")]
+        public static extern int getuid();
+    }
+}
