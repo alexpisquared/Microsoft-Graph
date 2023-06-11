40f4f7d2b5a31a0b855f990af0dad20af36ed801
alexpisqueared <alex.pigida@outlook.com>
2023-06-11 10:00:20 -0400

    ++

--- Src/msgraph-training-uwp/Win-App-calling-MsGraph/ClientId.cs
+++ Src/msgraph-training-uwp/Win-App-calling-MsGraph/ClientId.cs
@@ -11,10 +11,11 @@ class ClientId
   //   - for any Work or School accounts, or Microsoft personal account, use `common`
   //   - for Microsoft Personal account, use consumers
 
-  public const string ZoePiTry1 = "81b1c6c7-ea12-4466-841c-0c53b530330b"; // ZoeP_  // <== https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
-  public const string ZoePiTry2 = "751b8b39-cde8-44e5-91e4-020f42e86e95"; // ZoePi  // Created by/fot the QuickStart WPF app   WORKS for the WPF app
-  public const string NadinTry0 = "9ba0619e-3091-40b5-99cb-c2aca4abd04e"; // nadin 
-  public const string JingmTry1 = "6dc84e4e-68d0-4f11-ba48-7e468aecb270"; // jingm  // Try 1
-  public const string AlexpGood = "9ba0619e-3091-40b5-99cb-c2aca4abd04e"; // alex_  // MsgSlideshowUsrCtrl
-  public const string AlexpTest = "195390b6-cc9c-4294-a219-369d9e4cb9fa"; // alexp  // AppRegPocFixTry
+  public const string ZoePiTry1 = "81b1c6c7-ea12-4466-841c-0c53b530330b"; // <== https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
+  public const string ZoePiTry2 = "751b8b39-cde8-44e5-91e4-020f42e86e95"; // Created by/fot the QuickStart WPF app   WORKS for the WPF app
+  public const string NadinTry0 = "9ba0619e-3091-40b5-99cb-c2aca4abd04e"; 
+  public const string JingmTry1 = "6dc84e4e-68d0-4f11-ba48-7e468aecb270"; // Try 1
+  public const string JingmTry2 = "---------68d0-4f11-ba48-7e468aecb270"; // Try 2 
+  public const string AlexpGood = "9ba0619e-3091-40b5-99cb-c2aca4abd04e"; // MsgSlideshowUsrCtrl
+  public const string AlexpTest = "195390b6-cc9c-4294-a219-369d9e4cb9fa"; // AppRegPocFixTry
 }
