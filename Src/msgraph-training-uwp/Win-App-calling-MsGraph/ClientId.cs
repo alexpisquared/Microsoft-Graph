e10308a06095f7fd5920b631f8c8785bd2213766
alexpisqueared <alex.pigida@outlook.com>
2023-06-13 21:12:16 -0400

    a try to associate with my app; my client ifl etc.

--- Src/msgraph-training-uwp/Win-App-calling-MsGraph/ClientId.cs
+++ Src/msgraph-training-uwp/Win-App-calling-MsGraph/ClientId.cs
@@ -11,12 +11,14 @@
     //   - for any Work or School accounts, or Microsoft personal account, use `common`
     //   - for Microsoft Personal account, use consumers
 
-    public const string ZoePiTry1 = "81b1c6c7-ea12-4466-841c-0c53b530330b"; // <== https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
-    public const string ZoePiTry2 = "751b8b39-cde8-44e5-91e4-020f42e86e95"; // Created by/fot the QuickStart WPF app   WORKS for the WPF app
-    public const string NadinTry0 = "9ba0619e-3091-40b5-99cb-c2aca4abd04e";
-    public const string JingmTry1 = "6dc84e4e-68d0-4f11-ba48-7e468aecb270"; // Try 1
-    public const string JingmTry2 = "adcdfbad-d7c4-4df6-a1ab-eca20f7eb8a5"; // Try 2 
-    public const string AlexpGood = "9ba0619e-3091-40b5-99cb-c2aca4abd04e"; // MsgSlideshowUsrCtrl
-    public const string AlexpTest = "195390b6-cc9c-4294-a219-369d9e4cb9fa"; // AppRegPocFixTry
+    public const string
+      ZoePiTry1 = "81b1c6c7-ea12-4466-841c-0c53b530330b", // <== https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
+    ZoePiTry2 = "751b8b39-cde8-44e5-91e4-020f42e86e95", // Created by/fot the QuickStart WPF app   WORKS for the WPF app
+    NadinTry0 = "9ba0619e-3091-40b5-99cb-c2aca4abd04e",
+    JingmTry1 = "6dc84e4e-68d0-4f11-ba48-7e468aecb270", // Try 1
+    JingmTry2 = "adcdfbad-d7c4-4df6-a1ab-eca20f7eb8a5", // Try 2 
+    AlexpGood = "9ba0619e-3091-40b5-99cb-c2aca4abd04e", // MsgSlideshowUsrCtrl
+    AlexpTest = "195390b6-cc9c-4294-a219-369d9e4cb9fa", // AppRegPocFixTry
+    WellKnown = "4a1aa1d5-c567-49d0-ad0b-cd957a47f842"; // ==> APPARENTLY:  this is a well known client id for the Microsoft Graph Explorer demos
   }
 }
