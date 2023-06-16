03ada733908d9f6d85de6a4c537b5fc1d7c07d61
alexpisqueared <alex.pigida@outlook.com>
2023-06-16 17:53:58 -0400

    Client IDs for daughter , mom .

--- Src/msgraph-training-uwp/Win-App-calling-MsGraph/ClientId.cs
+++ Src/msgraph-training-uwp/Win-App-calling-MsGraph/ClientId.cs
@@ -14,6 +14,7 @@
     public const string
       ZoePiTry1 = "81b1c6c7-ea12-4466-841c-0c53b530330b", // <== https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
       ZoePiTry2 = "751b8b39-cde8-44e5-91e4-020f42e86e95", // Created by/fot the QuickStart WPF app   WORKS for the WPF app
+      ZoePiTryC = "37a70278-1484-49a3-902e-f10e06dd86f0", // Try00C
       NadinTry0 = "9ba0619e-3091-40b5-99cb-c2aca4abd04e", // fake
       JingmTry1 = "6dc84e4e-68d0-4f11-ba48-7e468aecb270", // Try 1
       JingmTry2 = "adcdfbad-d7c4-4df6-a1ab-eca20f7eb8a5", // Try002 
