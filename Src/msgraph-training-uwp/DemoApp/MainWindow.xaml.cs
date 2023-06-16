898654f877975332f46aed9936071f8d7306928c
alexpisqueared <alex.pigida@outlook.com>
2023-06-16 16:30:43 -0400

    +++

--- Src/msgraph-training-uwp/DemoApp/MainWindow.xaml.cs
+++ Src/msgraph-training-uwp/DemoApp/MainWindow.xaml.cs
@@ -7,7 +7,9 @@ public partial class MainWindow
   public MainWindow()
   {
     InitializeComponent();
-    MsgSlideshowUsrCtrl1.ClientId = Win_App_calling_MsGraph.ClientId.AlexpGood;
+
+    MsgSlideshowUsrCtrl1.ClientId = Win_App_calling_MsGraph.ClientId.JingmTry3;
+    MsgSlideshowUsrCtrl1.ClientNm = nameof(Win_App_calling_MsGraph.ClientId.JingmTry3);
   }
 
   void OnClose(object sender, RoutedEventArgs e) => Close();
